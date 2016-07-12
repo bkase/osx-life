acbd7b97fbf1490005e47b415f8d45fa132f6782
Brandon Kase <bkase@cmu.edu>
2016-07-11 23:02:57 -0700

    Initial commit

---
+++ default.nix
@@ -0,0 +1,58 @@
+with import <nixpkgs> { };
+
+let
+  # TODO: cleanup paths to custom packages
+  zshSyntaxHighlighting = callPackage ./nixos-config/etc/custom-packages/zsh-syntax-highlighting.nix {};
+  zshGitPrompt = callPackage ./nixos-config/etc/custom-packages/zsh-git-prompt/default.nix {};
+  scmpuff = callPackage ./nixos-config/etc/custom-packages/scmpuff.nix {};
+  fastarcheyosx = callPackage ./nixos-config/etc/custom-packages/fastarcheyosx.nix {};
+
+  vimrc = callPackage ./nixos-config/etc/vimrc.nix {};
+  vimrcConfig = {
+    vam.knownPlugins = pkgs.vimPlugins;
+    vam.pluginDictionaries = [
+      { names = [
+        "vim-addon-nix"
+        "ctrlp"
+        "youcompleteme"
+        "vim-airline"
+        "vim-airline-themes"
+        "surround"
+        "Solarized"
+        "The_NERD_Commenter"
+        #"rust-vim"
+
+        #"psc-ide-vim"
+        #"purescript-vim"
+        "syntastic"
+
+        "neomake"
+        "vimproc"
+        #"haskell-vim"
+        #"haskellConcealPlus"
+        #"ghcmod"
+        #"necoGhc"
+        #"hoogle"
+        #"hlint-refactor"
+      ];}
+    ];
+    customRC = vimrc.config;
+  };
+
+  myVim = pkgs.vim_configurable.customize { name = "vim"; inherit vimrcConfig; };
+in
+stdenv.mkDerivation rec {
+  name = "life";
+  version = "0.1.0";
+
+  buildInputs = [
+    myVim
+    git
+    fastarcheyosx
+    zsh
+    scmpuff
+    zshGitPrompt
+    zshSyntaxHighlighting
+    nix-zsh-completions
+  ];
+}
